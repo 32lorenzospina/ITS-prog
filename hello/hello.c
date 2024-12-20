9ff32f1dd854fa273205bcedda6618d6f8d50e15
32lorenzospina <32lorenzospina@gmail.com>
2024-12-20 16:08:47 +0000

    hello.c updated

--- hello/hello.c
+++ hello/hello.c
@@ -5,6 +5,17 @@ int main()
 
 printf("Hello WORLD!\n\n\n");
 
+char name [32];
+
+printf("Entere a name: ");
+
+
+scanf("%s, name"); // legge input dello user , lettura relativa alla stringa %s, elenaca nome variabile
+
+// stampa il mess con il nome scelto 
+printf("\nHello\t%s\n\n\n\,  name"); 
+
+
 return 0;
 
 }
