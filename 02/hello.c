4e55cd2abccc8608344346301ed0c5aeefdef1ab
32lorenzospina <32lorenzospina@gmail.com>
2025-01-21 15:22:39 +0000

    update hello02

---
+++ 02/hello.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#include <stdbool.h>
+
+
+#define degub_PRINT
+#define NEW_LINE printf("\n");
+#define PI_GRECO 3.14159265358;
+
+
+int main()
+{
+    printf("Hello WORLD!\n\n\n");
+
+
+    NEW_LINE;
+
+    char name[32]; 
+
+    int i =10;
+
+  
+
+    printf("Enter a name: ");
+
+    scanf("%s", name);  
+
+
+    printf("\nHello\t%s\n\n\n", name);
+
+    return 0;
+}
